8ab5b5f6a2227d9561e62bb9248ed80fc397c7d9
haein45 <ohhaein0405@gmail.com>
2025-01-27 22:11:28 +0900

    Feat: signin 닉네임 입력 기능추가

--- app/src/main/java/com/example/fe/SignUpComplete.kt
+++ app/src/main/java/com/example/fe/SignUpComplete.kt
@@ -1,6 +1,8 @@
 package com.example.fe
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +11,21 @@ import androidx.core.view.WindowInsetsCompat
 class SignUpComplete : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        // 먼저 setContentView() 호출
         setContentView(R.layout.activity_sign_up_complete)
+
+        // 이후 findViewById 호출
+        val nextButton: ImageButton = findViewById(R.id.next_button)
+        nextButton.setOnClickListener {
+            // NextActivity로 이동
+            val intent = Intent(this, SignUpPW::class.java)
+            startActivity(intent)
+        }
+
+        enableEdgeToEdge()
+
+        // 윈도우 인셋 적용
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
